ef28551e6542fa6899fe88723e8eee9543f52152
Emil Georgi <emil376g@edu.campusvejle.dk>
2018-08-27 09:20:13 +0200

    Add project files.

---
+++ AsyncServerClient/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace AsyncServerClient.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ AsyncServerClient/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AsyncServerClient.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
